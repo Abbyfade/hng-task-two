3fe2c9a9ccc0f3c3a1476896ff9bce16fcac36a9
User <Test@FADEYI-PC.lasuth.org.ng.local>
2022-11-05 22:26:29 +0100

    made changes

--- myapi/urls.py
+++ myapi/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Arithmetic
 app_name = "arithmetic"
 urlpatterns = [
-    path("api/", Arithmetic.as_view(), name="operation_list"),
+    path("api/", Arithmetic.as_view(), name="arithmetic"),
 ]
